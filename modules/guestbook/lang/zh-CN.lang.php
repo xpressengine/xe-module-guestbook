7b2eda6eb7e1a0b23570d481836762d6d913b91c
xe.master153 <xe.master153@gmail.com>
2011-08-09 03:22:07 +0000

    git-svn-id: http://xe-guestbook.googlecode.com/svn/trunk@5 1c21694a-e146-60e7-09b6-82ea964f0375

--- modules/guestbook/lang/zh-CN.lang.php
+++ modules/guestbook/lang/zh-CN.lang.php
@@ -1,22 +1,22 @@
 <?php
     /**
-     * @file   en.lang.php
+     * @file   zh-CN.lang.php
      * @author developer (developer@nhn.com)
      * @brief contact modules's basic language pack
      **/
 
-    $lang->guestbook = 'Guestbook';
+    $lang->guestbook = '留言簿';
 
-	$lang->guestbook_management = 'Guestbook Management';
-	$lang->about_guestbook = 'This module is for creating and managing Guestbook pages.';
+	$lang->guestbook_management = '留言簿管理';
+	$lang->about_guestbook = '此模块用于搭建和管理留言簿页面.';
 
-	$lang->cmd_guestbook_list = 'Guestbook Lists';
-	$lang->cmd_create_guestbook = 'Create a new Guestbook';
-    $lang->cmd_view_info = 'Guestbook Info';
+	$lang->cmd_guestbook_list = '留言簿列表';
+	$lang->cmd_create_guestbook = '新建留言簿';
+    $lang->cmd_view_info = '留言簿信息';
 
-	$lang->order_target = 'Sorting Target';
-	$lang->order_asc = 'Ascending order';
-	$lang->order_desc = 'Descending order';
+	$lang->order_target = '排序';
+	$lang->order_asc = '递增排序';
+	$lang->order_desc = '递减排序';
 	$lang->document_srl = 'Document No.';
 
 
