fb7b6ef6ed872a7ea8a9f9676113fd179ec9dd95
xe.master153 <xe.master153@gmail.com>
2011-08-09 03:12:18 +0000

    git-svn-id: http://xe-guestbook.googlecode.com/svn/trunk@4 1c21694a-e146-60e7-09b6-82ea964f0375

--- modules/guestbook/lang/ko.lang.php
+++ modules/guestbook/lang/ko.lang.php
@@ -1,22 +1,22 @@
 <?php
     /**
-     * @file   en.lang.php
+     * @file   ko.lang.php
      * @author developer (developer@nhn.com)
      * @brief contact modules's basic language pack
      **/
 
-    $lang->guestbook = 'Guestbook';
+    $lang->guestbook = '방명록';
 
-	$lang->guestbook_management = 'Guestbook Management';
-	$lang->about_guestbook = 'This module is for creating and managing Guestbook pages.';
+	$lang->guestbook_management = '방명록 관리';
+	$lang->about_guestbook = '이 모듈은 방명록 페이지 관리와 생성을 위한 것 입니다.';
 
-	$lang->cmd_guestbook_list = 'Guestbook Lists';
-	$lang->cmd_create_guestbook = 'Create a new Guestbook';
-    $lang->cmd_view_info = 'Guestbook Info';
+	$lang->cmd_guestbook_list = '방명록 리스트';
+	$lang->cmd_create_guestbook = '새 방명록 생성';
+    $lang->cmd_view_info = '방명록 정보';
 
-	$lang->order_target = 'Sorting Target';
-	$lang->order_asc = 'Ascending order';
-	$lang->order_desc = 'Descending order';
+	$lang->order_target = '정렬';
+	$lang->order_asc = '오름차순으로';
+	$lang->order_desc = '내림차순으로';
 	$lang->document_srl = 'Document No.';
 
 
